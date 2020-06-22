5a02088f418fb4e6846e8217ddce556f8acada4e
urihoenig <urih@iguaz.io>
2020-06-22 22:07:52 +0300

    Initial commit3 (#1)
    Initial commit

---
+++ jupyfm/jupyfm.py
@@ -0,0 +1,33 @@
+from notebook.services.contents.largefilemanager import LargeFileManager
+import os
+from tornado import web
+import shutil
+
+class jupyfm(LargeFileManager):
+    def rename_file(self, old_path, new_path):
+        """Rename a file."""
+        orig_old_path = old_path
+        old_path = old_path.strip('/')
+        new_path = new_path.strip('/')
+        if new_path == old_path:
+            return
+
+        new_os_path = self._get_os_path(new_path)
+        old_os_path = self._get_os_path(old_path)
+
+        # Should we proceed with the move?
+        if os.path.exists(new_os_path) and not samefile(old_os_path, new_os_path):
+            raise web.HTTPError(409, u'File already exists: %s' % new_path)
+
+        # Move the file
+        try:
+            with self.perm_to_403():
+                for i in os.scandir(path=orig_old_path):
+                    if i.is_dir():
+                        raise web.HTTPError(500, 'Renaming a folder with subfolders is not allowed: %s' % (old_path))
+                shutil.move(old_os_path, new_os_path)
+        except web.HTTPError:
+            raise
+        except Exception as e:
+            raise web.HTTPError(500, u'Unknown error renaming file: %s %s' % (old_path, e))
+
